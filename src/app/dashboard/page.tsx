9064c06fb0b5d982d44ddff7f5cfec62196f0daf
Rukchatpoom Wongsara <rukchatpoom.wongsara@gmail.com>
2024-03-29 14:21:07 +0700

    Dynamic routes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
+
+export default function Page() {
+    return <h1>Hello, Dashboard Pages2!</h1>
+  }
